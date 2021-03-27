d55f7c5a305375cb132a84e0795b8780002bda25
rinaldycodes <rinaldy.ramadhan999@gmail.com>
2021-03-27 16:27:02 +0700

    NAVBAR COMPLETE

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
 function App() {
-  return <h1>Portofolio</h1>;
+  return <Navbar />;
 }
 
 export default App;
